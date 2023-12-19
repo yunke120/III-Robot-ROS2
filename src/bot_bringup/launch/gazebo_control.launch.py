e62d8618f9e3d467c3c7a6fa5e0bfcc20c7d3109
yunke120 <fj_18860352816@163.com>
2023-12-19 13:55:32 +0800

    use gazebo_mecanum_plugin

--- src/bot_bringup/launch/gazebo_control.launch.py
+++ src/bot_bringup/launch/gazebo_control.launch.py
@@ -6,28 +6,27 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 
-# $ ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diffbot_base_controller/cmd_vel_unstamped
 
 def generate_launch_description():
 
     model = DeclareLaunchArgument(name="model", default_value=get_package_share_directory("bot_description") + "/urdf/bot.urdf.xacro")
-    p_value = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]))
+    description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]))
 
     robot_state_pub = Node(
                 package="robot_state_publisher",
                 executable="robot_state_publisher",
-                parameters=[{"robot_description":p_value}]
+                parameters=[{"robot_description": description}]
     )
 
-    # joint_state_pub = Node(
-    #     package="joint_state_publisher_gui",
-    #     executable="joint_state_publisher_gui",
-    # )
+    joint_state_pub = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+    )
 
     rviz2 = Node(
             package='rviz2',
@@ -49,16 +48,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"]
-    )
 
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diffbot_base_controller"]
-    )
 
-    return LaunchDescription([model, robot_state_pub, rviz2, gazebo, spawn_entity,joint_broad_spawner,diff_drive_spawner])
+    return LaunchDescription([model, joint_state_pub, robot_state_pub, rviz2, gazebo, spawn_entity])
